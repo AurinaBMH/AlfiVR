f2475d4f19818fa0009a7a550f153ccf4d58bfc8
Aurina <aurina.arnatk@gmail.com>
2020-07-31 13:45:18 +1000

    comments added

--- code/dataProcessing/collect_data.m
+++ code/dataProcessing/collect_data.m
@@ -1,6 +1,20 @@
-% This function will load data and make a big table for each subjects ectracting the relevant information;
+
 function dataALL = collect_data()
 
+% This function will load data and make a big table for each subjects ectracting the relevant information;
+% Aurina Arnatkeviciute 2020/07/24
+
+% -----------------------------------------------------------------------------
+% INPUT: 
+% no input required
+% -----------------------------------------------------------------------------
+
+% -----------------------------------------------------------------------------
+% OUTPUT: 
+% dataALL - table containing all data within data/analytics folder
+% -----------------------------------------------------------------------------
+
+% select where to take the data from
 fileList = dir('data/analytics');
 fileList = fileList(3:length(fileList),:);
 
@@ -10,37 +24,45 @@ whatMeasures = {'SessionNumber', 'Block', 'Trial', 'Trial_Type', ...
     'Cue_Accuracy', 'Adaptive_Stop_Signal_Offset', 'Stop_Signal_Delay',	...
     'responseTimeLeft', 'responseTimeRight', 'Avg_RespTime', 'L_R_Synchrony'};
 
-%Loop over all of the files in the "data" folder:
+% loop over all of the files in the "data" folder:
 dataSubject = cell(length(fileList),1); 
 for s = 1:length(fileList)
     
     fname = fileList(s).name;
+    % for files that are .analytics
     if contains(fname, '.analytics')
         
         % Get the data out of the .analytics file
         alfi_sess = convert_timeStamps(fname);
         numBlocks = length(alfi_sess.GameAnalytics.DragonSST.Blocks);
         
+        % this cell saves all data from one session
         dataSession = cell(numBlocks,1); 
+        
         for b = 1:numBlocks
             
             numTrials = size(alfi_sess.GameAnalytics.DragonSST.Blocks(b).Trials,1);
+            % this table will contain data for one selected block
             data = table;
-            % loop oerl selected measures and do something for each
-            % add a variable for subject
+            
+            % add a variable for subject, e.g. file name
             [subName{1:numTrials}] = deal(fname);
             data.File = subName'; 
             
+            % loop over selected measures and extract corresponding values
             for m=1:length(whatMeasures)
                 
                 switch whatMeasures{m}
                     case 'SessionNumber'
+                        % add the same session for all elements
                         data.(whatMeasures{m}) = repmat(alfi_sess.SessionNumber, numTrials,1);
                         
                     case 'Block'
+                        % add the same block for all elements
                         data.(whatMeasures{m}) = repmat(b, numTrials,1);
                         
                     case 'Trial'
+                        % variable for trials
                         data.(whatMeasures{m}) = linspace(1,numTrials, numTrials)';
                         
                     case {'Trial_Type', 'Stop_Signal_Left', 'Stop_Signal_Right', 'Cue_Type'}
@@ -52,6 +74,7 @@ for s = 1:length(fileList)
                                 selectData(t) = alfi_sess.GameAnalytics.DragonSST.Blocks(b).Trials(t).(whatMeasures{m});
                             end
                         end
+                        % add variables to table; 
                         data.(whatMeasures{m}) = selectData;
                         
                     case 'Stop_Signal_Delay'
@@ -67,25 +90,31 @@ for s = 1:length(fileList)
                                 
                             end
                         end
+                        % add variables to table; 
                         data.(whatMeasures{m}) = selectData;
+                        
                     case {'Avg_RespTime', 'L_R_Synchrony'}
+                        % calculate those based on L/R response times; 
                         selectData = nan(numTrials,1);
 
                         for t=1:numTrials
                             % if both responses exist
                             if ~isempty(alfi_sess.GameAnalytics.DragonSST.Blocks(b).Trials(t).responseTimeRight) && ...
                                     ~isempty(alfi_sess.GameAnalytics.DragonSST.Blocks(b).Trials(t).responseTimeLeft)
-                                % take the mean of L and R
+                                
                                 if strcmp(whatMeasures{m}, 'Avg_RespTime')
+                                % take the mean of L and R
                                     selectData(t) = mean([alfi_sess.GameAnalytics.DragonSST.Blocks(b).Trials(t).responseTimeRight,...
                                         alfi_sess.GameAnalytics.DragonSST.Blocks(b).Trials(t).responseTimeLeft]);
                                 elseif strcmp(whatMeasures{m}, 'L_R_Synchrony')
+                                % R-L in this case
                                     selectData(t) = alfi_sess.GameAnalytics.DragonSST.Blocks(b).Trials(t).responseTimeRight - ...
                                         alfi_sess.GameAnalytics.DragonSST.Blocks(b).Trials(t).responseTimeLeft;
                                 end
                                 
                             end
                         end
+                        % add variables to table; 
                         data.(whatMeasures{m}) = selectData;
                         
                     otherwise
@@ -98,15 +127,16 @@ for s = 1:length(fileList)
                                 
                             end
                         end
+                        % add variables to table; 
                         data.(whatMeasures{m}) = selectData;
                 end
                 
             end
-            
+            % data for each block added to the session cell
             dataSession{b} = data; 
-            % here add each table for a block at the end of existing big table
+            
         end
-        % for each subject add table into cell
+        % data from each file placed into a cell (these will be vertcat later)
         dataSubject{s} = vertcat(dataSession{:}); 
     end
 end
