14468103f251820ef82868d5e64839580b890268
Dan Myles <dan.myles@monash.edu>
2022-08-31 12:59:30 +1000

    Edited startup script to create data/analytics dir if absent
    Add 'dir' argument to exists()

--- startup.m
+++ startup.m
@@ -1,12 +1,28 @@
 % Add paths required for the project (ignoring hidden, including version control)
 function startup()
-files = dir;
-directories = files([files.isdir]);
+
+files = dir; % Get all folders and files at current directory
+
+% Check whether ./data/analytics folder is present in the directory
+if ~exist(fullfile(files(1).folder, 'data', 'analytics'), 'dir')  
+    % If not create it
+    mkdir(fullfile(files(1).folder, 'data', 'analytics'));
+
+    % Update files and directories lists
+    files = dir;
+end
+
+directories = files([files.isdir]); % Get folders
 directories(strmatch('.',{files([files.isdir]).name})) = []; % remove hidden
+
+% Generate path names for all subfolders
 paths = arrayfun(@(x)fullfile(directories(x).folder,directories(x).name),1:length(directories),'UniformOutput',false);
+
+% Add folders to matlab path
 for j = 1:length(paths)
     addpath(genpath(paths{j}))
 end
+
 end
 %-------------------------------------------------------------------------------
 % load('processedData.mat');
